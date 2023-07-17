8e767f532ef6d2456e141df85d4d4737642a31a6
Inito <inito@Bhaviks-MBP.local>
2023-07-17 14:08:22 +0530

    created new file to log output of prob_1b

--- week_1/prob_1b.sh
+++ week_1/prob_1b.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cat web-server.log | awk -F " " '{print $9}' | sort | uniq -c | awk '{print $2 ": " $1}'
+cat web-server.log | awk -F " " '{print $9}' | sort | uniq -c | awk '{print $2 ": " $1}' > output_prob_1b.txt
